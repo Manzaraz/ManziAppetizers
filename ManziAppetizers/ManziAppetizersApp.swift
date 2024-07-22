e8b3291c96cf224bf0ebff1379813155a0a419da
Christian Manzaraz <magnusmanz@gmail.com>
2024-07-22 12:33:11 -0300

    Initial Commit

---
+++ ManziAppetizers/ManziAppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ManziAppetizersApp.swift
+//  ManziAppetizers
+//
+//  Created by Christian Manzaraz on 22/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct ManziAppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
