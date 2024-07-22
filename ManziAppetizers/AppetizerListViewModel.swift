bf7b01c93ea68898594d5ce743c98089481b8a1a
Christian Manzaraz <magnusmanz@gmail.com>
2024-07-22 19:22:32 -0300

    Errors & Alerts

--- ManziAppetizers/AppetizerListViewModel.swift
+++ ManziAppetizers/AppetizerListViewModel.swift
@@ -10,14 +10,34 @@ import SwiftUI
 final class AppetizerListViewModel: ObservableObject {
     
     @Published var appetizers: [Appetizer] = []
+    @Published var alertItem: AlertItem?
     
     func getAppetizers() {
         NetworkManager.shared.getAppetizers { result in
             DispatchQueue.main.async {
+//            DispatchQueue.main.async { [self] in
                 switch result {
                 case .success(let appetizers):
                     self.appetizers = appetizers
+                    
                 case .failure(let error):
+                    switch error {
+                    case .invalidResponse:
+                        self.alertItem = AlertContext.invalidResponse
+//                        alertItem = AlertContext.invalidResponse
+                        
+                    case .invalidURL:
+                        self.alertItem = AlertContext.invalidURL
+//                        alertItem = AlertContext.invalidURL
+                        
+                    case .invalidData:
+                        self.alertItem = AlertContext.invalidData
+//                        alertItem = AlertContext.invalidData
+                        
+                    case .unableToComplete:
+                        self.alertItem = AlertContext.unableToComplete
+//                        alertItem = AlertContext.unableToComplete
+                    }
                     print(error.localizedDescription)
                 }
             }
